0ac081ace2423160923c91cf7d51415b9a97f4e2
Brent Marquez <brentonmarquez@gmail.com>
2019-10-13 21:49:01 -0600

    create interface for axios response in redux thunk action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,9 +1,17 @@
 import axios from "axios";
 import { Dispatch } from "redux";
 
+// define interface for the axios response (the shape of the data returned by the api):
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
-    const response = await axios.get(
+    // can pass in the response.data type/interface created (as an array since the api returns an array)
+    const response = await axios.get<Todo[]>(
       "https://jsonplaceholder.typicode.com/todos"
     );
     dispatch({
