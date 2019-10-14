92bfb1e90a9833fbb4cc0cacacc2eda0c310ee56
Brent Marquez <brentonmarquez@gmail.com>
2019-10-13 21:33:42 -0600

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface AppProps {
+  color?: string;
+}
+// interface AppState {
+//   counter: number;
+// }
+class App extends React.Component<AppProps /*AppState*/> {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default App;
