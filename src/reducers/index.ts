3d41cb73f59a9c4e2e524d7cd2c6c84f38b1d83c
Brent Marquez <brentonmarquez@gmail.com>
2019-10-13 22:47:58 -0600

    create store state interface

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,5 +1,18 @@
 import { combineReducers } from "redux";
+import { todosReducer } from "./todos";
+import { Todo } from "../actions";
 
-export const reducers = combineReducers({
-  counter: () => 1
+/**
+ * combineReducers is a generic function like dispatch and can take in a type describing the argument passed in.
+ *
+ * This ensures that the reducers return the expected data for state properties in the store.
+ * It prevents unintentional returning of incorrect data from a reducer
+ */
+// Create an interface to describe the entire state of the store
+interface StoreState {
+  todos: Todo[];
+}
+
+export const reducers = combineReducers<StoreState>({
+  todos: todosReducer
 });
